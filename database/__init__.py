2c3111bf4eaab69b4f4abf094f0845b9a7aa676b
ENKI <amit.khanna.1082@gmail.com>
2026-06-03 19:10:27 +0700

    Delete database/__init__.py

--- database/__init__.py
+++
@@ -1,5 +0,0 @@
-# database/__init__.py - Database module initialization
-from .database import engine, SessionLocal, Base
-from . import models, crud, schemas
-
-__all__ = ["engine", "SessionLocal", "Base", "models", "crud", "schemas"]
